4be01c7783d9453fbf557155d3811b4a4e26b7a3
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-08-18 20:35:11 +0300

    changed Main.java and Toy.java as added getter, setter and constructor

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) {
+        Toy firstToy = new Toy("First", 0.35);
+        Toy secondToy = new Toy("Second", 0.35);
+        Toy thirdToy = new Toy();
+        thirdToy.setName("Third");
+        thirdToy.setFrequencyOfLoss(0.5);
+
+        System.out.println(firstToy);
+        System.out.println(secondToy);
+        System.out.println(thirdToy);
+    }
+}
--- src/main/java/Toy.java
+++ src/main/java/Toy.java
@@ -1,5 +1,45 @@
+import java.util.UUID;
+
 public class Toy {
-    private String id;
+    private final String id;
     private String name;
     private Double frequencyOfLoss;
+
+    public Toy(String name, Double frequencyOfLoss) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.frequencyOfLoss = frequencyOfLoss;
+    }
+
+    public Toy() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId(){
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public Double getFrequencyOfLoss() {
+        return frequencyOfLoss;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFrequencyOfLoss(Double frequencyOfLoss) {
+        this.frequencyOfLoss = frequencyOfLoss;
+    }
+
+    @Override
+    public String toString() {
+        return "Toy{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", frequencyOfLoss=" + frequencyOfLoss +
+                '}';
+    }
 }
