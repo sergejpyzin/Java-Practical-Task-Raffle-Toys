5a11fb5df17b865af768b5fd29b55de7c38d7e5d
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-08-27 10:20:48 +0300

    changed UserInteraction as added checkingIntMassage method

--- src/main/java/UserInteraction.java
+++ src/main/java/UserInteraction.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class UserInteraction {
+
+    public int checkingIntMassage(String massage) {
+        Scanner scanner = new Scanner(System.in);
+        String userAnswer = scanner.next();
+        int number = 0;
+        try {
+           number = Integer.parseInt(userAnswer);
+           
+        } catch (NumberFormatException e){
+            System.out.println("Введенное значение не число " + e.getMessage());
+        }
+        return number;
+    }
 }
