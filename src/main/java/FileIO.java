20682b653b34343f83e2ee0eaa461d9738635066
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-08-28 10:18:56 +0300

    changed FileIO as crated is writeFile method

--- src/main/java/FileIO.java
+++ src/main/java/FileIO.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class FileIO {
 
-    void writeFile(String path){
+    public static void writeFile(List<Toy> toyList){
+        try {
+            FileWriter fileWriter = new FileWriter("test.txt");
+            for (Toy toy:toyList) {
+                String toys = toy.toString();
+                System.out.println(toys);
+                fileWriter.write(toys + "\n");
+            }
+//            fileWriter.flush();
+            fileWriter.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,30 +4,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        /*Toy firstToy = new Toy("First", 0.35);
+        Toy firstToy = new Toy("First", 0.35);
         Toy secondToy = new Toy("Second", 0.35);
         Toy thirdToy = new Toy();
 
         thirdToy.setName("Third");
         thirdToy.setFrequencyOfLoss(0.5);
 
-        System.out.println(firstToy);
-        System.out.println(secondToy);
-        System.out.println(thirdToy);
-
         List<Toy> toys = new ArrayList<>();
         toys.add(firstToy);
         toys.add(secondToy);
         toys.add(thirdToy);
         toys.add(new Toy("Four", 0.22));
-        System.out.println("==================================");
-        System.out.println(toys);
-        Collections.sort(toys);
-        System.out.println("-----------------------------------");
-        System.out.println(toys);
+        FileIO.writeFile(toys);
 
-    }*/
-        double number = UserInteraction.checkingUserAnswerForDouble("Введите число: ");
-        System.out.println(number);
     }
+        /*double number = UserInteraction.checkingUserAnswerForDouble("Введите число: ");
+        System.out.println(number);*/
+
 }
