a8617fe89142a5f06371032ab057de7f32187c1f
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-09-02 19:22:31 +0300

    changed Toy as changed Double frequencyOfLoss to String and constructor

--- src/main/java/Toy.java
+++ src/main/java/Toy.java
@@ -1,19 +1,16 @@
 import java.util.UUID;
 
 public class Toy implements Comparable<Toy>{
-    private final String id;
+    private String id;
     private String name;
-    private Integer frequencyOfLoss;
+    private String frequencyOfLoss;
 
-    public Toy(String name, int frequencyOfLoss) {
-        this.id = UUID.randomUUID().toString();
+    public Toy(String id, String name, String frequencyOfLoss) {
+        this.id = id;
         this.name = name;
         this.frequencyOfLoss = frequencyOfLoss;
     }
 
-    public Toy() {
-        this.id = UUID.randomUUID().toString();
-    }
 
     public String getId(){
         return id;
@@ -22,21 +19,25 @@ public class Toy implements Comparable<Toy>{
         return name;
     }
 
-    public int getFrequencyOfLoss() {
+    public String getFrequencyOfLoss() {
         return frequencyOfLoss;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setFrequencyOfLoss(int frequencyOfLoss) {
+    public void setFrequencyOfLoss(String frequencyOfLoss) {
         this.frequencyOfLoss = frequencyOfLoss;
     }
 
     @Override
     public String toString() {
-        return String.format("id:%s %s вероятность: %d", this.id, this.name, this.frequencyOfLoss);
+        return String.format("id: %s Наименование игрушки: %s шанс выпадения в процентах: %s", this.id, this.name, this.frequencyOfLoss);
     }
 
     @Override
